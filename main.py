5d00b96d4094dceaeedbbe75e573fb2e371f1f8f
PonnagantiHemanth <137759132+PonnagantiHemanth@users.noreply.github.com>
2024-04-18 12:21:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import unittest
 
-def addition(a,b):
-    return a+b
+def addition(a, b):
+    return a + b
+
 class TestAddition(unittest.TestCase):
     def test_positive(self):
-        result = addition(5,6)
-        self.assertEqual(result,11)
-    def test_negative(self):
-        result = addition(-5,-6)
-        self.assertEqual(result,result)
+        result = addition(5, 6)
+        self.assertEqual(result, 11)
+
 if __name__ == '__main__':
     unittest.main()
