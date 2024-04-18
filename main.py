00d5bfa763c72bc09e16bea72deeaa98e24f5177
PonnagantiHemanth <hemanthponnaganti1@gmail.com>
2024-04-18 11:09:30 +0530

    Edited

--- main.py
+++ main.py
@@ -1,47 +1,13 @@
-import logging
-import logging.handlers
-import os
+import unittest
 
-import requests
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger_file_handler = logging.handlers.RotatingFileHandler(
-    "status.log",
-    maxBytes=1024 * 1024,
-    backupCount=1,
-    encoding="utf8",
-)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-logger_file_handler.setFormatter(formatter)
-logger.addHandler(logger_file_handler)
-
-try:
-    SOME_SECRET = os.environ["SOME_SECRET"]
-except KeyError:
-    SOME_SECRET = "Token not available!"
-    #logger.info("Token not available!")
-    #raise
-
-
-if __name__ == "__main__":
-    logger.info(f"Token value: {SOME_SECRET}")
-
-    r = requests.get('https://weather.talkpython.fm/api/weather/?city=Berlin&country=DE')
-    if r.status_code == 200:
-        data = r.json()
-        temperature = data["forecast"]["temp"]
-        logger.info(f'Weather in Berlin: {temperature}')
-
-
-# def test_multiplication():
-#     print(a*b)
-#
-# def test_division():
-#     print(a/b)
-#
-# def test_exponential():
-#     print(a**b)
-#
-# def test_modulo():
-#     print(a%b)
+def addition(a,b):
+    return a+b
+class TestAddition(unittest.TestCase):
+    def test_positive(self):
+        result = addition(5,6)
+        self.assertEqual(result,11)
+    def test_negative(self):
+        result = addition(-5,-6)
+        self.assertEqual(result,result)
+if __name__ == '__main__':
+    unittest.main()
